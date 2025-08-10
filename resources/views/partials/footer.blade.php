048dc63f720ece03965662e2b09bfee4f975095c
Jhonnatas Mendonça <jhonnatas.m2000@gmail.com>
2025-08-10 19:49:26 -0300

    feat: desenvolvimento do footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<br>
+<br>
+<footer style="position: fixed; left: 0; bottom: 0; width: 100%; z-index: 100; background-color: #f8f9fa; border-top: 1px solid #dee2e6; border-radius: 0 0 15px 15px;">
+    <div class="text-center p-0">
+        <p>&copy; {{ date('Y') }} Açai Life. Todos os direitos reservados.</p>
+    </div>
+</footer>
